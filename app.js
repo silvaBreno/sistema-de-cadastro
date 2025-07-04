02e08e110e33dafad51ba77cc8c0912ee69c35bf
F1685239 Breno De Souza Silva <breno.souza@bb.com.br>
2025-07-04 10:17:44 -0300

    atualizando app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Rodando o sistema de gerenciamento de jogos");
+
+alert('Boas vindas ao nosso site!');
+
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+
+alert(mensagemDeErro);
+
+let userName = prompt('Insira seu nome?');
+let userAge = prompt('Digite a sua idade');
+
+if (userAge >= 18) {
+    alert('Pode tirar a habilitação!')
+}
